cc64d952889ece154982b309a0748a8e18a9f9d4
mohamedalaa1305 <mohamedalaaeldin2016@gmail.com>
2022-04-30 18:18:09 +0100

    configured database to work with identity

--- AngularAPI/Models/User.cs
+++ AngularAPI/Models/User.cs
@@ -1,40 +1,52 @@
 ﻿using AngularProject.Enums;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AngularProject.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        [Key]
-        public int Id { get; set; }
-        [StringLength(50), Required]
-        public string UserName { get; set; }
-        [Required, DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
-        [Required, DataType(DataType.Password)]
-        public string Password { get; set; }
 
+        //[Key]
+        //public int Id { get; set; }
+
+        //[StringLength(50), Required]
+        //public string UserName { get; set; }
+        
+        
+        //[Required, DataType(DataType.EmailAddress)]
+        //public string Email { get; set; }
+        
+        
+        //[Required, DataType(DataType.Password)]
+        //public string Password { get; set; }
+
+        
         [Required, EnumDataType(typeof(Gender))]
         public Gender Gender { get; set; }
+        
+        
         [Required]
         public string Address { get; set; }
 
-        [Required, ForeignKey("Role")]
-        public int RoleId { get; set; }
+        
+        //[Required, ForeignKey("Role")]
+        //public int RoleId { get; set; }
 
-        [Required]
+        
+        //[Required]
         public int CartID { get; set; }
+
 
         [ForeignKey("Image")]
         public int ImageId { get; set; }
-        public Image Image { get; set; }
-
-        public Role Role { get; set; }
+        
+        //public Role Role { get; set; }
+        public virtual Image? Image { get; set; }
+        public virtual Cart Cart { get; set; }
 
         //// list of orders
-        public ICollection<Order> Orders { get; set; }
-
-
+        public virtual ICollection<Order>? Orders { get; set; }
     }
 }
--- AngularAPI/Program.cs
+++ AngularAPI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ShoppingDb>(
+builder.Services.AddDbContext<ShoppingDbContext>(
     options => options.UseSqlServer(
                builder.Configuration.GetConnectionString("ShopDbConn")
     ));
