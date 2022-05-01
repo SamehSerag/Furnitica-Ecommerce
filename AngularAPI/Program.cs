a4384edddf66b9778e8bda06a68783d9055d67d9
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2022-05-01 21:07:07 +0200

    Add Product Repository

--- AngularAPI/Program.cs
+++ AngularAPI/Program.cs
@@ -1,3 +1,4 @@
+using AngularAPI.Repository;
 using AngularProject.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductRepository, ProductService>();
 
 builder.Services.AddDbContext<ShoppingDbContext>(
     options => options.UseSqlServer(
---
+++ AngularAPI/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using AngularProject.Models;
+
+namespace AngularAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IReadOnlyList<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<Product> UpdateProductAsync(int id, Product product);
+        Task AddProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+        bool IsProductExixtsAsync(int id);
+
+    }
+}
