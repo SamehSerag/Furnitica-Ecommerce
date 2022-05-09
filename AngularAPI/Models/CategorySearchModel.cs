bbd5379e017ddb99349afb1944d53dcc74b8c19f
omnia-fathy <61743696+omnia-fathy@users.noreply.github.com>
2022-05-09 20:02:52 +0200

    Update CategorySearchModel.cs

--- AngularAPI/Models/CategorySearchModel.cs
+++ AngularAPI/Models/CategorySearchModel.cs
@@ -4,13 +4,11 @@
     {
         //property to sort by 
         public string? Sort { get; set; }
-    
         //search string
         public string? Search { get; set; }
         //page index of pagination
         public int PageIndex { get; set; } = 1;
         //items per page
         public int PageSize { get; set; } = 10;
-
     }
 }
