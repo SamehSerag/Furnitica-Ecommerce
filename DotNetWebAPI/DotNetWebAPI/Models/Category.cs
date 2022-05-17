85d2d4b2c1bf67e60b3108552d23aa941304549c
omnia-fathy <61743696+omnia-fathy@users.noreply.github.com>
2022-05-18 01:39:42 +0200

    Category and Role seeds

--- DotNetWebAPI/DotNetWebAPI/Models/Category.cs
+++ DotNetWebAPI/DotNetWebAPI/Models/Category.cs
@@ -10,6 +10,8 @@ namespace AngularProject.Models
         public string Name { get; set; }
 
         public string? Image { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     
     }
 }
