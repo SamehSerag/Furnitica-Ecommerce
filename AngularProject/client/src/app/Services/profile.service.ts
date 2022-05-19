e3d51afe9ee131eb3ac1fd7db0d657cbb12ab1b1
mohamedalaa1305 <mohamedalaaeldin2016@gmail.com>
2022-05-19 17:03:35 +0100

    updit auth ui + added user profile ui

---
+++ AngularProject/client/src/app/Services/profile.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, Observable, ObservableInput, tap } from "rxjs";
+import { environment } from "src/environments/environment";
+import { IUser } from "../Models/IUser";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProfileService {
+
+  static errMessage : string = "";
+  static statusCode : number = 200;
+
+  constructor(private http: HttpClient) { }
+
+  getProfile () : Observable<any> {
+    let headers : HttpHeaders = new HttpHeaders().set("Authorization", "Bearer " + localStorage["access-token"]);
+    let url = `${environment.BaseUrl}/User`;
+    let res = this.http.get<any>(url, {headers, observe: "response"} ).pipe (
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.habdleError)
+    );
+    return res;
+  }
+
+  private habdleError(err : any) : ObservableInput<any> {
+    console.log("handling err: ", err);
+    console.log("status-code = ", err["status"]);
+    console.log("message = ", err["error"]);
+    ProfileService.statusCode = err["status"];
+    ProfileService.errMessage = err["error"];
+    let res = {} as ObservableInput<any>;
+    return res;
+  }
+
+  updateProfile (user : IUser) : Observable<any> {
+    let headers : HttpHeaders = new HttpHeaders().set("Authorization", "Bearer " + localStorage["access-token"]);
+    let url = `${environment.BaseUrl}/User`;
+    let res = this.http.put<any>(url, user, {headers, observe: "response"} ).pipe (
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.habdleError)
+    );
+    return res;
+  }
+
+}
