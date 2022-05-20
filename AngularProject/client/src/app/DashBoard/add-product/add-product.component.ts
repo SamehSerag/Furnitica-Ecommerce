e0704e18f5c2c3f04358f4621de05a8573db4854
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2022-05-20 02:04:21 +0200

    d7k

--- AngularProject/client/src/app/DashBoard/add-product/add-product.component.ts
+++ AngularProject/client/src/app/DashBoard/add-product/add-product.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ICategory } from 'src/app/Models/icategories';
+import { IProduct } from 'src/app/Models/iproduct';
+import { IOwnerProduct } from 'src/app/Models/Owner/IOwnerProduct';
+import { IProductToAdd } from 'src/app/Models/Owner/IProductToAdd';
 import { CategoryService } from 'src/app/Services/category.service';
+import { ProductsService } from 'src/app/Services/products.service';
 
 @Component({
   selector: 'app-add-product',
@@ -8,9 +13,39 @@ import { CategoryService } from 'src/app/Services/category.service';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-  constructor(private categoryService:CategoryService,private router:Router) { }
+  productToAdd!: IProductToAdd;
+  categoriesList!: ICategory[];
+
+  constructor(private categoryService:CategoryService, private prdService: ProductsService,
+    private router:Router) { 
+      // this.categoryService.getAllCategories().subscribe((response)=>{
+      //   this.categoriesList = response;
+      //   console.log(this.categoriesList)
+      // });
+    }
 
   ngOnInit(): void {
+    this.categoryService.getAllCategories().subscribe((response)=>{
+      this.categoriesList = response;
+      console.log(this.categoriesList)
+    });
+  }
+
+  AddProduct() {
+    const observer = {
+      next: (prd: IOwnerProduct) => {
+        alert("Added Successfully");
+        // this.productsSearchModel.pageIndex = this.pagination.totalPages;
+        // this.ngOnInit();
+        // this.scrollToBottom();
+      },
+      Error: (prd: IOwnerProduct) => {
+        alert("Error !!");
+      }
+
+    }
+    // this.prdService.addProduct(this.productToAdd).subscribe(observer);
+    console.log(this.productToAdd);
 
   }
 
