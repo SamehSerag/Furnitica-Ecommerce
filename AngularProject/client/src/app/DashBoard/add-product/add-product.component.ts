2a54c296894fe0a8ae9dff241bf0820e1b91045a
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2022-05-20 02:49:15 +0200

    add product without image is done

--- AngularProject/client/src/app/DashBoard/add-product/add-product.component.ts
+++ AngularProject/client/src/app/DashBoard/add-product/add-product.component.ts
@@ -4,6 +4,7 @@ import { ICategory } from 'src/app/Models/icategories';
 import { IProduct } from 'src/app/Models/iproduct';
 import { IOwnerProduct } from 'src/app/Models/Owner/IOwnerProduct';
 import { IProductToAdd } from 'src/app/Models/Owner/IProductToAdd';
+import { ProductToAdd } from 'src/app/Models/Owner/ProductToAdd';
 import { CategoryService } from 'src/app/Services/category.service';
 import { ProductsService } from 'src/app/Services/products.service';
 
@@ -13,11 +14,15 @@ import { ProductsService } from 'src/app/Services/products.service';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-  productToAdd!: IProductToAdd;
+  productToAdd!: ProductToAdd;
   categoriesList!: ICategory[];
 
   constructor(private categoryService:CategoryService, private prdService: ProductsService,
     private router:Router) { 
+      this.productToAdd = new ProductToAdd();
+      this.productToAdd.ownerId = "1";
+      this.productToAdd.categoryID = 1;
+
       // this.categoryService.getAllCategories().subscribe((response)=>{
       //   this.categoriesList = response;
       //   console.log(this.categoriesList)
@@ -38,14 +43,16 @@ export class AddProductComponent implements OnInit {
         // this.productsSearchModel.pageIndex = this.pagination.totalPages;
         // this.ngOnInit();
         // this.scrollToBottom();
+        this.router.navigate([`/Products/${prd.id}`])
+
       },
       Error: (prd: IOwnerProduct) => {
         alert("Error !!");
       }
 
     }
-    // this.prdService.addProduct(this.productToAdd).subscribe(observer);
-    console.log(this.productToAdd);
+    this.prdService.addProduct(this.productToAdd).subscribe(observer);
+    // console.log(this.productToAdd);
 
   }
 
---
+++ AngularProject/client/src/app/Models/Owner/ProductToAdd.ts
@@ -0,0 +1,11 @@
+export class ProductToAdd {
+    title_EN!: string;
+    title_AR!: string;
+    details_EN!: string;
+    details_AR!: string;
+    price!: number;
+    color!: number;
+    quantity!: number;
+    categoryID!: number;
+    ownerId!: string;
+}
