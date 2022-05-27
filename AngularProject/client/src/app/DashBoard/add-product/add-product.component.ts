9a27216f7793043229e92f83c39a62f3e862f3b7
mohamedalaa1305 <mohamedalaaeldin2016@gmail.com>
2022-05-27 05:17:38 +0100

    added validation for add product

--- AngularProject/client/src/app/DashBoard/add-product/add-product.component.ts
+++ AngularProject/client/src/app/DashBoard/add-product/add-product.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpEventType } from '@angular/common/http';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { ICategory } from 'src/app/Models/icategories';
-import { IProduct } from 'src/app/Models/iproduct';
+// import { IProduct } from 'src/app/Models/iproduct';
 import { IOwnerProduct } from 'src/app/Models/Owner/IOwnerProduct';
 import { IProductToAdd } from 'src/app/Models/Owner/IProductToAdd';
 import { ProductToAdd } from 'src/app/Models/Owner/ProductToAdd';
@@ -17,12 +17,13 @@ import { ProductsService } from 'src/app/Services/products.service';
 export class AddProductComponent implements OnInit {
   progress: number = 0;
   message: string = "";
-
   productToAdd!: ProductToAdd;
   categoriesList!: ICategory[];
   @ViewChild('imageInput') imageInput!: ElementRef;
   onUploadFinished: any;
   colorArray: string[];
+
+  isValidated : boolean = false;
 
   constructor(private categoryService: CategoryService, private prdService: ProductsService,
     private router: Router, private http: HttpClient) {
@@ -30,7 +31,36 @@ export class AddProductComponent implements OnInit {
     this.productToAdd.ownerId = "1";
     this.productToAdd.categoryID = 1;
     this.colorArray = ["Blue", "Green", "Yellow", "Brown", "Pink", "Red"];
+
+    // this.imageInput.nativeElement.quer
+    // this.imageInput.nativeElement.querySelector('#images')
+    // .addEventListener('change', ()=>{
+    //   console.log("changed!!!!!!!!!!!!!!!!");
+    // });
+
+    // this.productToAdd = new ProductToAdd();
+    // this.productToAdd.categoryID = this.categoriesList[0].id;
+    this.productToAdd.color = 1;
+    this.productToAdd.details_AR = "";
+    this.productToAdd.details_EN = "";
+    this.productToAdd.title_AR = "";
+    this.productToAdd.title_EN = "";
+    this.productToAdd.quantity = 0;
+    this.productToAdd.price = 0;
+    console.log("product to add: ", this.productToAdd);
   }
+
+  validate() : boolean{
+    this.isValidated = true;
+    return this.productToAdd.title_AR.length > 0
+    && this.productToAdd.title_EN.length > 0
+    && this.productToAdd.details_AR.length > 0
+    && this.productToAdd.details_EN.length > 0
+    && this.productToAdd.price > 0
+    && this.productToAdd.quantity >= 0
+    && this.imageInput.nativeElement.files.length !== 0;
+  }
+
 
   ngOnInit(): void {
     this.categoryService.getAllCategories().subscribe((response) => {
@@ -40,6 +70,9 @@ export class AddProductComponent implements OnInit {
   }
 
   AddProduct() {
+    if(!this.validate())
+      return;
+
     const observer = {
       next: (prd: IOwnerProduct) => {
         alert("Added Successfully");
@@ -122,7 +155,7 @@ export class AddProductComponent implements OnInit {
 
 //     for (let i = 0; i < filesToUpload.length; i++) {
 //       formData.append("files", filesToUpload[i]);
-//     }    
+//     }
 //     formData.append("price", `${1000}`);
 //     formData.append("name", `${this.productToAdd.title_EN}`);
 //     this.http.post('https://localhost:44376/api/images/AddImages2', formData)
